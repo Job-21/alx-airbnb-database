f196f396ac31c65c37777b9d70d87e29bf598ae3
JOB-21 <jobabe211@gmail.com>
2025-07-21 15:58:05 +0300

    added a new aggregation file

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -3,9 +3,16 @@ SELECT user_id, COUNT(*) AS total_bookings
 FROM bookings
 GROUP BY user_id;
 
--- 2. Rank properties based on the total number of bookings they have received using ROW_NUMBER()
+-- 2. Rank properties based on total bookings using ROW_NUMBER()
 SELECT property_id,
        COUNT(*) AS total_bookings,
-       ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+       ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_num_rank
+FROM bookings
+GROUP BY property_id;
+
+-- 3. Rank properties based on total bookings using RANK()
+SELECT property_id,
+       COUNT(*) AS total_bookings,
+       RANK() OVER (ORDER BY COUNT(*) DESC) AS rank_order
 FROM bookings
 GROUP BY property_id;
